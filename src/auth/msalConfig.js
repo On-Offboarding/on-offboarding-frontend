d3e583df722385c5efd76fd06b1ee8276b3ed2c3
jjo <jjo@axogroup.com>
2026-04-23 10:44:44 +0200

    update secrets

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -4,7 +4,7 @@ export const msalInstance = new PublicClientApplication({
   auth: {
     clientId: "44a194a3-d54b-41e4-905f-05d9f9094e34",
     authority: "https://login.microsoftonline.com/f622d913-8a09-415f-a525-b65e4ba829c5/v2.0",
-    redirectUri: "http://localhost:5173"
+    redirectUri: import.meta.env.VITE_REDIRECT_URI || "http://localhost:5173"
   },
   cache: {
     cacheLocation: "sessionStorage",
